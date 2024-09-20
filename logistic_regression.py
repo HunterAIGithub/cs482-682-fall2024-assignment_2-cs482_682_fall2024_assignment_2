d646d551f105cd4dee73e78fa70c8fa4b78f825f
HunterAIGithub <hafrick@unr.edu>
2024-09-19 19:59:56 -0700

    Problem 1 Done.

--- logistic_regression.py
+++ logistic_regression.py
@@ -8,8 +8,8 @@ class MyLogisticRegression:
     def __init__(self, dataset_num, perform_test):
         self.training_set = None
         self.test_set = None
-        self.model_logistic = None
-        self.model_linear = None
+        self.model_logistic = LogisticRegression()
+        self.model_linear = LinearRegression()
         self.X_train = None
         self.y_train = None
         self.X_test = None
@@ -20,37 +20,49 @@ class MyLogisticRegression:
         self.read_csv(self.dataset_num)
 
     def read_csv(self, dataset_num):
-        if dataset_num == '1':
+        if self.dataset_num == '1':
             train_dataset_file = 'train_q1_1.csv'
             test_dataset_file = 'test_q1_1.csv'
-        elif dataset_num == '2':
+        elif self.dataset_num == '2':
             train_dataset_file = 'train_q1_2.csv'
             test_dataset_file = 'test_q1_2.csv'
         else:
             print("unsupported dataset number")
-            
+
         self.training_set = pd.read_csv(train_dataset_file, sep=',', header=0)
         if self.perform_test:
             self.test_set = pd.read_csv(test_dataset_file, sep=',', header=0)
         
         
     def model_fit_linear(self):
-        '''
-        initialize self.model_linear here and call the fit function
-        '''
+        y_train = self.training_set['label']
+        x_train = self.training_set[['exam_score_1', 'exam_score_2']]
+
+        if self.perform_test:
+            self.y_test = self.test_set['label']
+            self.X_test = self.test_set[['exam_score_1', 'exam_score_2']]
+
+        self.model_linear.fit(x_train, y_train)
         pass
     
     def model_fit_logistic(self):
         '''
-        initialize self.model_logistic here and call the fit function
+        Initialize self.model_logistic here and call the fit function.
         '''
+        y_train = self.training_set['label']
+        x_train = self.training_set[['exam_score_1', 'exam_score_2']]
 
-        pass
+        if self.perform_test:
+            self.y_test = self.test_set['label']
+            self.X_test = self.test_set[['exam_score_1', 'exam_score_2']]
+
+        self.model_logistic.fit(x_train, y_train)
     
     def model_predict_linear(self):
         '''
         Calculate and return the accuracy, precision, recall, f1, support of the model.
         '''
+        self.read_csv(1)
         self.model_fit_linear()
         accuracy = 0.0
         precision, recall, f1, support = np.array([0,0]), np.array([0,0]), np.array([0,0]), np.array([0,0])
@@ -59,6 +71,9 @@ class MyLogisticRegression:
         
         if self.X_test is not None:
             # perform prediction here
+            self.model_linear.predict(self.X_test)
+            r_score = self.model_linear.score(self.X_test, self.y_test)
+            print("Linear Regression Accuracy: " + str(r_score))
             pass
         
         assert precision.shape == recall.shape == f1.shape == support.shape == (2,), "precision, recall, f1, support should be an array of shape (2,)"
@@ -75,6 +90,9 @@ class MyLogisticRegression:
         assert self.training_set is not None, "self.read_csv function isn't called or the self.trianing_set hasn't been initialized "
         if self.X_test is not None:
             # perform prediction here
+            self.model_logistic.predict(self.X_test)
+            score = self.model_logistic.score(self.X_test, self.y_test)
+            print("Logistic Accuracy: " + str(score))
             pass
         
         assert precision.shape == recall.shape == f1.shape == support.shape == (2,), "precision, recall, f1, support should be an array of shape (2,)"
