d5ad5ee68da00c3fed44904b3300c73e851e07ba
HunterAIGithub <hafrick@unr.edu>
2024-09-19 20:34:07 -0700

    Problem 2 done

--- kmeans_clustering.py
+++ kmeans_clustering.py
@@ -1,31 +1,39 @@
 import numpy as np
 import argparse
 import scipy.io
+import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
-class MykmeansClustering:
+class MyKMeansClustering:
     def __init__(self, dataset_file):
         self.model = None
-        
         self.dataset_file = dataset_file
+        self.X = None
+
         self.read_mat()
 
     def read_mat(self):
         mat = scipy.io.loadmat(self.dataset_file)
-        
-    def model_fit(self):
+        self.X = mat['X']
+
+    
+    def model_fit(self, n_clusters=3, max_iter=300):
         '''
-        initialize self.model here and execute kmeans clustering here
+        Initialize self.model here and execute kmeans clustering here
         '''
-        
-        cluster_centers = np.array([[0,0]])
+        self.model = KMeans(n_clusters=n_clusters, max_iter=max_iter, random_state=42)
+        self.model.fit(self.X)
+
+        cluster_centers = self.model.cluster_centers_
         return cluster_centers
 
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Kmeans clustering')
-    parser.add_argument('-d','--dataset_file', type=str, default = "dataset_q2.mat", help='path to dataset file')
+    parser = argparse.ArgumentParser(description='K-means clustering')
+    parser.add_argument('-d','--dataset_file', type=str, default="dataset_q2.mat", help='path to dataset file')
     args = parser.parse_args()
-    classifier = MykmeansClustering(args.dataset_file)
-    clusters_centers = classifier.model_fit()
-    print(clusters_centers)
+    classifier = MyKMeansClustering(args.dataset_file)
+    
+    clusters_centers = classifier.model_fit(n_clusters=3)
+    print("Cluster Centers:\n", clusters_centers)
     
